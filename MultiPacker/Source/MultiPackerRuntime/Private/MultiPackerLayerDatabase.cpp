f592f3082f5d575f4d4383372f83619e5f9245e8
mastercoms <mastercoms@tuta.io>
2020-02-11 13:18:18 -0500

    remove multipacker

--- MultiPacker/Source/MultiPackerRuntime/Private/MultiPackerLayerDatabase.cpp
+++
@@ -1,7 +0,0 @@
-/* Copyright 2019 TurboCheke, Estudio Cheke  - All Rights Reserved */
-#include "MultiPackerLayerDatabase.h"
-
-UMultiPackerLayerDatabase::UMultiPackerLayerDatabase(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
--- MultiPacker/Source/MultiPackerRuntime/Private/TileUtils/TileCopy.cpp
+++
@@ -1,7 +0,0 @@
-/* Copyright 2019 TurboCheke, Estudio Cheke  - All Rights Reserved */
-#include "TileUtils/TileCopy.h"
-
-UTileCopy::UTileCopy(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
