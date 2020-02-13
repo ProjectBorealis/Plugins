cff7dd3900a5ed824142ea662f0a0080dd3a2e94
mastercoms <mastercoms@tuta.io>
2020-02-13 18:45:40 -0500

    add ECS plugin

---
+++ ECS/Source/ECS.Build.cs
@@ -0,0 +1,21 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ECS : ModuleRules
+{
+	public ECS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        // UEBuildConfiguration.bForceEnableExceptions = true;
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        bEnableExceptions = true;
+        PublicIncludePaths.AddRange(
+            new string[] {
+
+                        "ECS/ThirdParty",
+                        "ECS/ECS_Base"
+            }
+        );
+    }
+}
---
+++ ECS/Source/ECS.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "ECS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ECS, "ECS");
