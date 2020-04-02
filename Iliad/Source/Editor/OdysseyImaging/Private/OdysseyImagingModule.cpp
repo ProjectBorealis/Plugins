e68c3f2d09b137dd333e58832f2b99c734ace0ce
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2020-04-02 17:46:24 +0300

    Remove Iliad

--- Iliad/Source/Editor/OdysseyImaging/Private/OdysseyImagingModule.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright © 2018-2019 Praxinos, Inc. All Rights Reserved.
-// IDDN FR.001.250001.002.S.P.2019.000.00000
-
-#include "CoreMinimal.h"
-#include "Modules/ModuleManager.h"
-#include "OdysseySurface.h"
-
-IMPLEMENT_MODULE( FDefaultModuleImpl, OdysseyImaging );
--- Iliad/Source/Editor/OdysseyWidgets/Private/OdysseyWidgetsModule.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright © 2018-2019 Praxinos, Inc. All Rights Reserved.
-// IDDN FR.001.250001.002.S.P.2019.000.00000
-
-#include "CoreMinimal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FDefaultModuleImpl, OdysseyWidgets);
