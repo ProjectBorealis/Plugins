8a0ed96aecc8675ecc799cd4bab1a7021dc476d7
mastercoms <mastercoms@tuta.io>
2020-07-17 08:59:42 -0400

    remove substance (doesn't load properly)

--- Runtime/Substance/Source/SubstanceLiveLink/Private/SubstanceLiveLinkPrivatePCH.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 Allegorithmic Inc. All rights reserved.
-// File: SubstanceLiveLinkPrivatePCH.h
-#pragma once
-#include "Logging/LogMacros.h"
-
-DEFINE_LOG_CATEGORY_STATIC(LogSubstanceLiveLink, Log, All);
