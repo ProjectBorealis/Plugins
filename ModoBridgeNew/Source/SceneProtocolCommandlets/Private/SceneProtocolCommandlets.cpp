cf4fef294de65d57e97c9424106335a0dc26a8ed
mastercoms <mastercoms@tuta.io>
2020-11-30 12:34:51 -0500

    remove modo

--- ModoBridgeNew/Source/SceneProtocolCommandlets/Private/SceneProtocolCommandlets.cpp
+++
@@ -1,9 +0,0 @@
-// --------------------------------------------------------------------------
-// Defines plugin commandlets log category.
-//
-// Copyright (c) 2018 The Foundry Visionmongers Ltd. All Rights Reserved.
-// --------------------------------------------------------------------------
-#include "SceneProtocolCommandlets.h"
-
-DEFINE_LOG_CATEGORY(LogSceneProtocolCommandlets);
-
